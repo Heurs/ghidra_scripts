dfc4620219cb18213df02500e6e8900604bb07cd
Heurs <stefan.le-berre@exatrack.com>
2020-06-22 17:28:52 +0200

    Registers and logs review

--- EmulateIt.py
+++ EmulateIt.py
@@ -11,13 +11,13 @@ emuHelper = ghidra.app.emulator.EmulatorHelper(currentProgram)
 # emuHelper.writeMemory(toAddr(0x00000000000CF000), b'\x99\xAA\xBB\xCC')
 
 reg_filter = [
-    "RAX", "RBX", "RCX", "RDX", "RSI", "RDI", 
+    "RAX", "RBX", "RCX", "RDX", "RSI", "RDI", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15",
     "RSP", "RBP", "rflags"
 ]
 
-reg_state = ["RAX", "RBX", "RCX", "RDX", "RSI", "RDI", 
+reg_state = ["RAX", "RBX", "RCX", "RDX", "RSI", "RDI", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15",
     "RSP", "RBP", "rflags"]
-reg_state_prev = ["RAX", "RBX", "RCX", "RDX", "RSI", "RDI", 
+reg_state_prev = ["RAX", "RBX", "RCX", "RDX", "RSI", "RDI", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15",
     "RSP", "RBP", "rflags"]
 
 import random
@@ -64,13 +64,6 @@ while monitor.isCancelled() is False:
         cinstr = getInstructionAt(executionAddress)
         
         print("0x{} {}".format(executionAddress, cinstr))
-        for i in range(len(reg_filter)):
-            creg = reg_filter[i]
-            prev_val = reg_state[i]
-            reg_val = emuHelper.readRegister(creg)
-            if reg_val != prev_val:
-                print("  {} = {:#018x}".format(creg, reg_val))
-                reg_state[i] = reg_val
         if cinstr.getMnemonicString() in handled_instructions:
             handled_instructions[cinstr.getMnemonicString()]()
             mainFunctionEntry = currentAddress
@@ -79,6 +72,13 @@ while monitor.isCancelled() is False:
             success = True
         else:
             success = emuHelper.step(monitor)
+        for i in range(len(reg_filter)):
+            creg = reg_filter[i]
+            prev_val = reg_state[i]
+            reg_val = emuHelper.readRegister(creg)
+            if reg_val != prev_val:
+                print("  {} = {:#018x}".format(creg, reg_val))
+                reg_state[i] = reg_val
         if (success == False):
             lastError = emuHelper.getLastError()
             printerr("Emulation Error: '{}'".format(lastError))
